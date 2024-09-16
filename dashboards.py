c923b5b1a15ca9bcfee1d741b357423989ed0c2b
Marcos Aquino <marcos.aquino@unifesp.br>
2024-09-16 13:28:35 -0300

    adicionando comentários e limpando o código

--- dashboards.py
+++ dashboards.py
@@ -5,8 +5,8 @@ import openpyxl
 import xlrd
 import copy
 import numpy as np
-from dateutil.parser import parse
 
+#função para categorizar os dados: direção do vento em graus na rosa dos ventos
 def categorizar_direcao_16(dict_direcoes, graus, velocidade):
     if 0 <= graus < 11.25 or 348.75 <= graus <= 360:
         dict_direcoes['N']['Frequencia'] += 1
@@ -57,7 +57,7 @@ def categorizar_direcao_16(dict_direcoes, graus, velocidade):
         dict_direcoes['NNO']['Frequencia'] += 1
         categorizar_velocidade(dict_direcoes['NNO'], velocidade)
 
-
+#função para categorizar a velocidade do vento de acordo com a direção
 def categorizar_velocidade(dict_velocidade, velocidade):
     if 0 <= velocidade < 0.5:
         dict_velocidade['Calmaria'] += 1
@@ -74,10 +74,11 @@ def categorizar_velocidade(dict_velocidade, velocidade):
     elif velocidade >= 11.1:
         dict_velocidade['>= 11.1'] += 1
 
+#definição inicial do layout da página
 st.set_page_config(layout="wide")
 
+#pede para o usuário escolher o arquivo excel. Válido adicionar o formato csv depois
 st.write("Análise de dados metereológicos")
-#if csv
 tipo = st.selectbox("Escolha o tipo do arquivo excel: ", ['xls', 'xlsx'])
 upload_file = st.file_uploader(f"Escolha o arquivo excel {tipo}: ", type = [tipo])
 
@@ -88,10 +89,11 @@ elif tipo == 'xlsx':
 else:
     ferramenta = ''
 
+#se recebeu um arquivo válido
 if upload_file is not None and ferramenta != '':
     df = pd.read_excel(upload_file, engine=ferramenta)
-    #convertendo colunas em string, tratando comma e depois convertendo para float
-    for coluna in df.iloc[:, list(range(2, 13)) + [15]]:
+    #convertendo colunas em string, tratando a vírgula (,) e depois convertendo para float
+    for coluna in df.iloc[:, list(range(2, 13)) + [15]]: #os dados da coluna 13 e 14 não serão utilizados no momento
         df[coluna] = df[coluna].astype(str)
         df[coluna] = df[coluna].str.replace(',', '.').astype(float)
         df[coluna] = pd.to_numeric(df[coluna], errors='coerce')
@@ -102,6 +104,7 @@ if upload_file is not None and ferramenta != '':
 
     df['Hora'] = pd.to_datetime(df['Hora'], format='%H:%M').dt.time
 
+    #streamlit não reconheceu a biblioteca locale, foi necessário realizar essa conversão manualmente:
     meses_pt = {
     'January de 2024': 'Janeiro de 2024',
     'February de 2024': 'Fevereiro de 2024',
@@ -125,44 +128,50 @@ if upload_file is not None and ferramenta != '':
     df['Day'] = df['Data'].dt.strftime('%d')
     day = st.sidebar.selectbox("Dia", sorted(df["Day"].unique()))
 
+    #dois filtros = mês e dia, para análise mais precisa dos dados
     dados_filtrados = df[(df["Month"] == month) & (df["Day"] == day)]
 
-    #ordenar por hora
+    #ordenar os dados filtrados por hora
     dados_filtrados = dados_filtrados.sort_values('Hora')
 
-    # Criando um novo DataFrame sem as colunas a serem ocultadas
-    colunas_a_ocultar = ['Month', 'Day', 'Bateria (V)', 'Temperatura Interna (ºC)']
+    # As colunas ocultadas na apresentação do dataframe são: colunas_a_ocultar = ['Month', 'Day', 'Bateria (V)', 'Temperatura Interna (ºC)']
     st.dataframe(dados_filtrados.iloc[:, list(range(0, 13))+ [15]])
 
+    #plotagem de gráficos de linha, com marcadores para cada 15 min
     for coluna in df.iloc[:, list(range(2, 6)) + [7, 9, 10, 12, 15, 11]]: 
         # Verificando se há dados para plotar
         if dados_filtrados.empty:
-            st.warning("Não há dados para o dia selecionado.")
+            st.warning("Não há dados para o dia selecionado.") 
         else:
-            # Criação do gráfico com Plotly
+            # Criação do gráfico
             fig_date = px.line(dados_filtrados, x='Hora', y=df[coluna].name, 
                             title=f'{df[coluna].name} do Dia {day} de {month}',
                             markers=True)
             
             st.plotly_chart(fig_date, use_container_width=True)
 
+    #informações do dataframe em graus
     for coluna in df.iloc[:, [6, 8]]:
         if dados_filtrados.empty:
             st.warning("Não há dados para o dia selecionado.")
         else:
             col1, col2 = st.columns(2) #divide a tela em 2 
 
-            
             with col1:
+                #primeiro o gráfico de linha correspondente
                 fig3 = px.line(dados_filtrados, x='Hora', y=df[coluna].name, 
                             title=f'{df[coluna].name} - {day} de {month}',
                             markers=True)
                 st.plotly_chart(fig3, use_container_width=True)
 
+            #plotagem da rosa dos ventos com 16 direções
             with col2:
-                with st.container():           
+                with st.container():
+                    #filtro de 6 em 6 horas, ou dia inteiro           
                     horas_6 = st.selectbox('Selecione o horário:', [f'Dia inteiro: {df[coluna].name}', f'0h-5h45: {df[coluna].name}', f'6h-11h45: {df[coluna].name}', f'12h-17h45: {df[coluna].name}', f'18h-23h45: {df[coluna].name}'])
                     
+                    #cada direção recebe um dicionário de velocidades
+                    #deepcopy é recomendado para elementos mais complexos, pois garante que todos os níveis da estrutura sejam copiados de forma independente.
                     dict_vel = {'Frequencia': 0, 'Calmaria': 0, '0.5 - 2.1': 0, '2.1 - 3.6': 0, '3.6 - 5.7': 0, '5.7 - 8.8': 0, '8.8 - 11.1': 0, '>= 11.1': 0}
                     dict_direcoes = {'N': copy.deepcopy(dict_vel), 'NNE': copy.deepcopy(dict_vel), 'NE': copy.deepcopy(dict_vel), 'ENE': copy.deepcopy(dict_vel), 'E': copy.deepcopy(dict_vel), 'ESE': copy.deepcopy(dict_vel), 'SE': copy.deepcopy(dict_vel), 'SSE': copy.deepcopy(dict_vel),
                                      'S': copy.deepcopy(dict_vel), 'SSO': copy.deepcopy(dict_vel), 'SO': copy.deepcopy(dict_vel), 'OSO': copy.deepcopy(dict_vel), 'O': copy.deepcopy(dict_vel), 'ONO': copy.deepcopy(dict_vel), 'NO': copy.deepcopy(dict_vel), 'NNO': copy.deepcopy(dict_vel)}
@@ -171,6 +180,7 @@ if upload_file is not None and ferramenta != '':
                     dados_filtrados['Data_str'] = dados_filtrados['Data'].astype(str)
                     dados_filtrados['Hora_str'] = dados_filtrados['Hora'].astype(str) 
                     
+                    #encontrar o índice do dataframe para o mês, dia e hora 00h do filtro, para gerar a rosa dos ventos
                     linha_00h = dados_filtrados[dados_filtrados['Hora_str'] == '00:00:00']
                     if not linha_00h.empty:
                         indice_00h = linha_00h.index[0]  # Pega o primeiro índice, caso haja múltiplas linhas
@@ -183,7 +193,6 @@ if upload_file is not None and ferramenta != '':
                         dict_direcoes = {'N': copy.deepcopy(dict_vel), 'NNE': copy.deepcopy(dict_vel), 'NE': copy.deepcopy(dict_vel), 'ENE': copy.deepcopy(dict_vel), 'E': copy.deepcopy(dict_vel), 'ESE': copy.deepcopy(dict_vel), 'SE': copy.deepcopy(dict_vel), 'SSE': copy.deepcopy(dict_vel),
                                          'S': copy.deepcopy(dict_vel), 'SSO': copy.deepcopy(dict_vel), 'SO': copy.deepcopy(dict_vel), 'OSO': copy.deepcopy(dict_vel), 'O': copy.deepcopy(dict_vel), 'ONO': copy.deepcopy(dict_vel), 'NO': copy.deepcopy(dict_vel), 'NNO': copy.deepcopy(dict_vel)}
                         total = 0
-
 
                         for indice, linha in enumerate(dados_filtrados[coluna][0:96]):
                             categorizar_direcao_16(dict_direcoes, linha, dados_filtrados['Velocidade do Vento (m/s)'][indice_00h+indice])
@@ -229,6 +238,7 @@ if upload_file is not None and ferramenta != '':
                             categorizar_direcao_16(dict_direcoes, linha, dados_filtrados['Velocidade do Vento (m/s)'][indice_00h+indice+72])
                             total +=1             
                     
+                    #separando as informações de direção, velocidade e frequência para tratar como dataframe
                     data = []
                     for direcao, info in dict_direcoes.items():
                         for velocidade, frequencia in info.items():
@@ -236,9 +246,13 @@ if upload_file is not None and ferramenta != '':
                                 data.append({'Direção': direcao, 'Velocidade': velocidade, 'Frequencia': frequencia})
 
                     df2 = pd.DataFrame(data)
+                    #normalização em porcentagem da frequencia obtida de cada direção e velocidade
                     df2['Frequencia: '] = (round((df2['Frequencia'] / total) * 100, 2))
 
+                    #cores para a legenda de velocidades
                     colors = ['lightblue', 'blue', 'purple', 'green', 'yellow', 'orange', 'red']
+                    
+                    #rosa dos ventos é um gráfico polar, com barras
                     fig = px.bar_polar(df2, r="Frequencia: ", theta="Direção",
                                     color="Velocidade",
                                     color_discrete_sequence=colors,
@@ -246,7 +260,6 @@ if upload_file is not None and ferramenta != '':
                                     title=f"Rosa dos Ventos {df[coluna].name} - {horas_6}",
                                     barmode='stack')
 
-                    
                     st.plotly_chart(fig, use_container_width=True)    
    
 else:
